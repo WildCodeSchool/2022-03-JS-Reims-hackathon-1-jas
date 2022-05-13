ff8a38467307ac6b83bb29c24f8f2edaccbb9395
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-05-13 10:33:59 +0200

    added mascot templates to the remaining pages

--- frontend/src/pages/Connect.jsx
+++ frontend/src/pages/Connect.jsx
@@ -5,6 +5,11 @@ export default function Connect() {
   return (
     <>
       <Header />
+      <img
+        src="src/assets/images/mister-bin-laptop.png"
+        alt="mascot-laptop"
+        className="mascot-laptop"
+      />
       <form className="login">
         <div className="email">
           Email : <input id="email" name="email" type="email" />
