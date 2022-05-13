c77cb0db1b41f82887343730caf036afdb4b4d5e
Simon Duc <ducsimon@gmail.com>
2022-05-13 11:12:08 +0200

    styled the search page

--- frontend/src/pages/HomeBin.jsx
+++ frontend/src/pages/HomeBin.jsx
@@ -23,33 +23,35 @@ export default function HomeBin() {
   return (
     <>
       <Header />
-      <h1>Votre recherche</h1>
-      <p>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        Pas besoin de se déplacer ! Un(e) bouteille d'eau est un déchet à jeter
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        dans la poubelle d'ordures recyclables de votre immeuble (poubelle
-        jaune). Voici ci-dessous les dates de levée des poubelles jaunes dans
-        votre commune :
-      </p>
-      {wasteCollection && (
-        <ul>
-          <li>
-            Type de collecte : {wasteCollection.datasetid},{" "}
-            {wasteCollection.fields.type_collecte.toLowerCase()}
-          </li>
-          <li>Semaine paire ou impaire : {wasteCollection.fields.semaine}</li>
-          <li>Dates de collecte : {wasteCollection.fields.jour}</li>
-        </ul>
-      )}
-      <div className="did-you-know">
-        <h3>Le saviez-vous ?</h3>
+      <div className="homebin">
+        <h1>Votre recherche</h1>
         <p>
-          Un Français consomme en moyenne 96 bouteilles plastiques par an !
-          Chaque jour, 25 millions de bouteilles sont jetées dans le pays. Pour
-          en finir avec ces chiffres vertigineux, sachez enfin que la production
-          de plastique a été multipliée par 50 en 50 ans en France.
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          Pas besoin de vous déplacer ! La bouteille d'eau est un déchet à jeter
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          dans la poubelle d'ordures recyclables de votre immeuble (poubelle
+          jaune). Voici ci-dessous les dates de levée des poubelles jaunes dans
+          votre commune :
         </p>
+        {wasteCollection && (
+          <ul>
+            <li>
+              Type de collecte : {wasteCollection.datasetid},{" "}
+              {wasteCollection.fields.type_collecte.toLowerCase()}
+            </li>
+            <li>Semaine paire ou impaire : {wasteCollection.fields.semaine}</li>
+            <li>Dates de collecte : {wasteCollection.fields.jour}</li>
+          </ul>
+        )}
+        <div className="did-you-know">
+          <h3>Le saviez-vous ?</h3>
+          <p>
+            Un Français consomme en moyenne 96 bouteilles plastiques par an !
+            Chaque jour, 25 millions de bouteilles sont jetées dans le pays.
+            Pour en finir avec ces chiffres vertigineux, sachez enfin que la
+            production de plastique a été multipliée par 50 en 50 ans en France.
+          </p>
+        </div>
       </div>
     </>
   );
