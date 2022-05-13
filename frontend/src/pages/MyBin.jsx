50ab009f640d382be0598f0e0bc405b53811cdb4
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-05-13 03:05:22 +0200

    Advanced structure

--- frontend/src/pages/MyBin.jsx
+++ frontend/src/pages/MyBin.jsx
@@ -1,53 +1,67 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import Toggle from "react-toggle";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Map from "../components/Map";
+import Header from "../components/Header";
 
 export default function MyBin() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [Toggle, setToggle] = useState(true);
+  const [landfill, setLandfill] = useState();
+
+  function pickLandfill() {
+    axios
+      .get(
+        "https://epn-agglo.opendatasoft.com/api/records/1.0/search/?dataset=assist_vocaux_dechetterie&q="
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setLandfill(data.records[4]);
+      });
+  }
+
+  useEffect(() => {
+    pickLandfill();
+  }, []);
 
   return (
     <>
-      <Link to="/" />
+      <Header />
       <h2>Bonjour Simon !</h2>
-      <div className="around-me">Les points de récoltes autour de vous</div>
-      <div className="notifications">
-        <h4>Gestion des notifications</h4>
-        Souhaitez-vous modifiez vos notifications
-      </div>
-      <div className="poubelle-verte">
-        Notification poubelle verte
-        <Toggle name="toggle-pv" value="yes" />
-      </div>
-      <div className="poubelle-jaune">
-        Notification poubelle jaune
-        <Toggle id="toggle-pj" defaultChecked="true" onChange="false" />
-      </div>{" "}
-      <div className="le-saviez-vous">
-        Notification Le-saviez-vous
-        <Toggle id="" defaultChecked="true" onChange="false" />
-      </div>
-      <Button onClick={handleOpen}>Modifiez votre adresse</Button>
-      <Modal className="modal-adresse" open={open} onClose={handleClose}>
-        <Box>
-          <Typography className="adresse-actuelle">
-            Votre adresse actuelle : 73 rue du Docteur Thomas 27025 Quelque part
-            France
-          </Typography>
-          <Typography className="">
-            <label htmlFor="edit-adresse">
-              Nouvelle adresse :
-              <input type="text" id="nouvelle-adresse" />
-            </label>
-            <Button onClick={handleClose}>Enregistrer</Button>
-          </Typography>
-        </Box>
-      </Modal>
+      <main className="main-mybin">
+        <div className="around-me">
+          Voici les différents points de récoltes autour de vous
+          {landfill && (
+            <Map
+              landfillAddress={landfill.fields.nom}
+              landfillCity={landfill.fields.commune}
+              landfillLongitude={landfill.fields.longitude}
+              landfillLatitude={landfill.fields.latitude}
+              landfillSchedule={landfill.fields.horaires}
+            />
+          )}
+        </div>
+        <div className="notifications">
+          <h4>Gestion des notifications</h4>
+          Souhaitez-vous modifiez vos notifications
+        </div>
+        <button
+          type="button"
+          className="notification-verte"
+          onClick={() => setToggle((prevState) => !prevState)}
+        >
+          {Toggle
+            ? "Recevoir un rappel pour la collecte de déchets ménagers"
+            : "Ne pas recevoir de rappels pour la collecte de déchets ménagers"}
+        </button>
+        <button
+          type="button"
+          onClick={() => setToggle((prevState) => !prevState)}
+        >
+          {Toggle
+            ? "Recevoir un rappel pour la collecte de déchets recyclables"
+            : "Ne pas recevoir de rappels pour la collecte de déchets recyclables"}
+        </button>
+        <button type="button">Modifiez votre adresse</button>
+      </main>
     </>
   );
 }
